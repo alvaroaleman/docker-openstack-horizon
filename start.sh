6c64194b5b693e4edcb4054cee3526fb3e99cf4f
Alvaro Aleman <alv2412@googlemail.com>
2016-07-25 01:27:38 +0200

    Automatically set no of WSGI processses to no of cpus

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -xeuo pipefail
 
+# Set Keystone URL
 sed -i 's/^OPENSTACK_KEYSTONE_URL.*/OPENSTACK_KEYSTONE_URL = "'$KEYSTONE_URL'"/g' $HORIZON_BASEDIR/openstack_dashboard/local/local_settings.py
+
+# Set WSGI Worker proccesses
+export NUM_CPU=$(cat /proc/cpuinfo |grep processor|wc -l)
+sed -E -i 's/(.*WSGIDaemonProcess openstack_dashboard_website).*/\1 processes='${NUM_CPU}'/g' /etc/apache2/sites-enabled/horizon.conf
 
 export APACHE_RUN_USER=www-data
 export APACHE_RUN_GROUP=www-data
